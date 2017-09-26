2b8fd3fcf131dc374205519b1af65cc7cc110440
ALEX87 <sa.erira@uniandes.edu.co>
2017-09-26 16:32:46 -0500

    Optimizacion del Componente LOGIN

--- src/components/common/login/login.js
+++ src/components/common/login/login.js
@@ -1,3 +1,10 @@
+/*
+Nombre del Componente:  Login
+Descripcion:            Captura datos de usuario para validarlos contra la base de datos y
+                        permitir el aceso a usuarios con una cuenta creada.
+Estado:                 En desarollo
+Desarrollador:          Steven Alexander Erira Fierro
+*/
 import React from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
@@ -45,13 +52,18 @@ class login extends React.Component {
                             hintText="Ingrese su nombre de usuario"
                             floatingLabelText="Ingrese su nombre de usuario"
                             id="usuario"
+                            fullWidth={true}
                         /><br />
                         <TextField
                             hintText="Ingrese su contraseña"
                             floatingLabelText="Contraseña"
                             type="password"
                             id = "password"
-                        />
+                            fullWidth={true}
+                        /><br />
+                        <div style={{fontSize:'12px', fontWeight:'bold', color:'#000000'}}>
+                            <br />Si no ha creado una cuenta de usuario, por favor cierre esta ventana y haga clic en el botón REGISTRO
+                        </div>
                     </div>
                 </Dialog>
             </div>
