d9c206f77f6e333d7d7693706b8e08e44e75c2a1
ALEX87 <sa.erira@uniandes.edu.co>
2017-11-28 14:51:52 -0500

    Se corrigen opciones de pago y boton de confirmacion de compra

--- src/components/common/Cart/Cart.js
+++ src/components/common/Cart/Cart.js
@@ -10,10 +10,10 @@ import {List, ListItem} from 'material-ui/List';
 import Close from 'material-ui/svg-icons/navigation/close';
 import Avatar from 'material-ui/Avatar';
 import Checkbox from 'material-ui/Checkbox';
-import ActionFavorite from 'material-ui/svg-icons/action/favorite';
-import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
-import Visibility from 'material-ui/svg-icons/action/visibility';
-import VisibilityOff from 'material-ui/svg-icons/action/visibility-off';
+//import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+//import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
+//import Visibility from 'material-ui/svg-icons/action/visibility';
+//import VisibilityOff from 'material-ui/svg-icons/action/visibility-off';
 
 import './Cart.css';
 
